869d191801dcea1fe63d00dffe812642a9e5ebe8
Mihai G <75206574+MikeGh66@users.noreply.github.com>
2025-12-16 16:31:23 +0200

    update files

---
+++ scrap.py
@@ -0,0 +1,17 @@
+# name = "1234"
+
+# print(len(name))
+
+# print(type(name))
+
+
+# print(type(1234))
+
+# print(type(True))
+# print(type(12.4))
+
+# print (int(123)+int(124))
+
+name=input("What is your name? ")
+len_name=len(name)
+print ("Number of letters in your name is:"+ str(len_name))
